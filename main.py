2bf6db31f29aa8d3b1fe51d997a2a1d567bbf3e5
david <ddleon@outlook.co.il>
2019-02-20 16:36:36 +0200

    in development

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 
 import os
 import re #Regular Expression lib
+import pandas as pd
 
 cwd = os.getcwd()
+NULL_IDX = -1
 
-
+'''
+Return -1 if there is no regex of [Number] 
+Return Reference number if regex of [Number] exists
+'''
 def check_if_contain_ref_num(name):
     regex = re.compile(r'\[(\d*)\]')
     match = regex.search(name)
     if match != None:
-        return match.start() == 0
-    return False
+        ref_string = name[match.start():match.end()]
+        ref_string = ref_string.replace("[","").replace("]","")
+        return int(ref_string)
+    return NULL_IDX
 
 def get_pdf_files():
     pdf_files = []
@@ -19,10 +26,15 @@ def get_pdf_files():
             pdf_files.append(filename)
     return pdf_files
 
-
 articles_list = get_pdf_files()
+existed_ref_indexes = []
+series = pd.Se
 
 for pdf_file in articles_list:
+    ref_status = check_if_contain_ref_num(pdf_file)
+    if(ref_status == NULL_IDX):
+        existed_ref_indexes.append(ref_status)
+
     res = check_if_contain_ref_num(pdf_file)
 
 
